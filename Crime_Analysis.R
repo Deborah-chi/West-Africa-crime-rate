12fb39a180b9b289c27c26e71b1c17e79b409030
Deborah-chi <deborahelesie@gmail.com>
2026-09-18 14:46:00 +0100

    This shows the Data Types and Data Structure of the Objects

--- Crime_Analysis.R
+++ Crime_Analysis.R
@@ -1,4 +1,8 @@
 
+# Load Packages ------------------------------------------------------------
+
+library(dplyr)
+
 # Create object country ---------------------------------------------------
 
 country <- c(rep("Nigeria", 3), "Cameroon",
@@ -16,5 +20,65 @@ city <- c("Maiduguri", "Unknown", "Kano", "Bamenda",
   "Buea", "Buni Yadi", "Damboa", "Gamboru"
   )
 
-n_crime <- c(448, 225, 130, 87, 76, 65, 63, 55, 51,
+n_crime <- c(488, 225, 130, 87, 76, 65, 63, 55, 51,
   46, 42, 38, 37, 36, 35, 32, 31, 30, 30, 29)
+
+
+# Data Types and Structures -----------------------------------------------
+
+## Data Type
+typeof(country)
+typeof(city)
+typeof(n_crime)
+
+## Data Structure
+str(country)
+str(city)
+str(n_crime)
+
+
+# Total Number of Crime Committed ------------------------------------------
+
+sum(n_crime)
+
+crime_tbl <- data.frame(
+  country,
+  city,
+  n_crime
+)
+
+crime_tbl$crime_rate <- ifelse(
+  test = crime_tbl$n_crime >= 400,
+  yes = "High",
+  no = ifelse(
+    test = between (crime_tbl$n_crime, 100, 399),
+    yes = "Medium",
+    no = "Low"
+  )
+)
+
+## Second method
+crime_tbl$crime_rate <- if_else(
+  condition = crime_tbl$n_crime >= 400,
+  true = "High",
+  false = if_else(
+    condition = between(crime_tbl$n_crime, 100, 399),
+    true = "Medium",
+    false = "Low"
+  )
+)
+
+crime_tbl |> 
+  summarise(
+    .by = country,
+    total_crime = sum(n_crime),
+    average_number_of_crime = mean(n_crime)
+  )
+
+
+crime_tbl |> 
+  summarise(
+    .by = country,
+    total_crime = sum(n_crime),
+  ) |> 
+  top_n(n = 1, wt = total_crime)
